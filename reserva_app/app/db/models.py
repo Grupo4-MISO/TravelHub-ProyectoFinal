241dce927febe2588bc4148ce862ac5fdade230a
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2026-04-24 16:13:30 -0500

    Borramos clase de Payment en el modelo de datos

--- reserva_app/app/db/models.py
+++ reserva_app/app/db/models.py
@@ -15,15 +15,3 @@ class ReservaORM(db.Model):
     estado = db.Column(db.String(20), nullable = False, default = 'pendiente')
     created_at = db.Column(db.DateTime, nullable = False, default = db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, nullable = False, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
-
-class Payment(db.Model):
-    __tablename__ = 'payments'
-
-    id = db.Column(UUID(as_uuid = True), primary_key = True, default = uuid.uuid4)
-    reserva_id = db.Column(UUID(as_uuid = True), db.ForeignKey('reservas.id'), nullable = False)
-    amount = db.Column(db.Float, nullable = False) # El monto total a pagar
-    currency = db.Column(db.String(3), nullable = False) # Ejemplo: 'USD', 'COP', etc.
-    payment_method = db.Column(db.String(50), nullable = False) # Ejemplo: 'tarjeta_credito', 'paypal', etc.
-    status = db.Column(db.String(20), nullable = False, default = 'pendiente')
-    created_at = db.Column(db.DateTime, nullable = False, default = db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, nullable = False, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
