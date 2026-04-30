0d98ec6043dc377ff21e23e3f1ff175a1b237abc
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2026-04-29 23:08:02 -0500

    ci: update SQLAlchemy database URI configuration across multiple apps

--- autenticadorapp/main.py
+++ autenticadorapp/main.py
@@ -12,7 +12,8 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 #Creamos la aplicacion de Flask
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+#Ponemos configuraciones de la app
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///travelhub.db")
 app.config['JWT_SECRET_KEY'] = 'o+jGoFFM5+EZULQUkXUkmxNU9eGSxU89GlCG9hbNSYI='
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
